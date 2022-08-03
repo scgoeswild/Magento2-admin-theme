a26ef4c83c465be29789415f371cc12a8cd99370
Simone Chinaglia <hello@simonechinaglia.net>
2022-08-03 05:55:15 +0200

    release 1.0.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.4',
+        'version' => '1.0.4.0',
+        'type' => 'magento2-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'simonechinaglia/admintheme',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'simonechinaglia/admintheme' => array(
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'type' => 'magento2-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
